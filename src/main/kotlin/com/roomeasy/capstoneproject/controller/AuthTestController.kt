19ed1abd6362cf299c6abb58d0abaca8bb91863a
gunom <gu3062@naver.com>
2023-06-30 18:00:31 +0900

    [FEAT] 테스트 수정

--- src/main/kotlin/com/roomeasy/capstoneproject/controller/AuthTestController.kt
+++ src/main/kotlin/com/roomeasy/capstoneproject/controller/AuthTestController.kt
@@ -1,19 +1,24 @@
 package com.roomeasy.capstoneproject.controller
 
 import com.roomeasy.capstoneproject.common.auth.JwtTokenProvider
+import com.roomeasy.capstoneproject.common.enums.JWTExceptionCode
 import com.roomeasy.capstoneproject.controller.dto.AuthTestRequestDto
 import com.roomeasy.capstoneproject.controller.dto.LoginResponse
 import com.roomeasy.capstoneproject.controller.dto.RefreshTestRequestDto
 import com.roomeasy.capstoneproject.repository.user.UserRepository
 import io.jsonwebtoken.Claims
+import io.jsonwebtoken.ExpiredJwtException
 import io.jsonwebtoken.JwtException
+import io.jsonwebtoken.MalformedJwtException
 import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.CrossOrigin
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import java.security.SignatureException
+import javax.servlet.ServletRequest
+import javax.servlet.ServletResponse
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
 
 @RestController
 @RequestMapping("/api/test/auth")
@@ -24,27 +29,74 @@ class AuthTestController(
 ) {
     @PostMapping("/login")
     fun test(@RequestBody requestBody: AuthTestRequestDto): ResponseEntity<LoginResponse> {
-        val token = jwtTokenProvider.generateTokenForTest(requestBody.accessTokenExpiredTime, requestBody.refreshTokenExpiredTime)
+        val token = jwtTokenProvider.generateTokenForTest(
+            requestBody.accessTokenExpiredTime,
+            requestBody.refreshTokenExpiredTime
+        )
         return ResponseEntity.ok().body(
             LoginResponse(
-                HttpStatus.OK.value(),true,"로그인 성공", token.first, token.second
-        ))
+                HttpStatus.OK.value(), true, "로그인 성공", token.first, token.second
+            )
+        )
     }
 
     @PostMapping("/refresh")
-    fun refreshTest(@RequestBody requestBody: RefreshTestRequestDto): ResponseEntity<LoginResponse> {
-        if(!jwtTokenProvider.validateRefreshToken(requestBody.refreshToken)){
-            throw JwtException("Invalid refresh token")
-        }
-        val claims: Claims = jwtTokenProvider.getUserIdFromRefreshToken(requestBody.refreshToken).getOrThrow()
+    fun refreshTest(
+        @RequestBody requestBody: RefreshTestRequestDto,
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+    ): ResponseEntity<LoginResponse> {
+            jwtTokenProvider.validateRefreshToken(requestBody.refreshToken)
+        val claims: Claims =
+            jwtTokenProvider.getUserIdFromRefreshToken(requestBody.refreshToken).getOrThrow()
         val userId: Long = claims["id"]?.let {
             userRepository.findById((it as Int).toLong()).get().id
         } ?: throw JwtException("Failed to retrieve user details from JWT token")
 
-        val token = jwtTokenProvider.generateTokenForTest(requestBody.accessTokenExpiredTime, requestBody.refreshTokenExpiredTime)
+        val token = jwtTokenProvider.generateTokenForTest(
+            requestBody.accessTokenExpiredTime,
+            requestBody.refreshTokenExpiredTime
+        )
         return ResponseEntity.ok().body(
             LoginResponse(
-                HttpStatus.OK.value(),true,"토큰 재발급 성공", token.first, token.second
-        ))
+                HttpStatus.OK.value(), true, "토큰 재발급 성공", token.first, token.second
+            )
+        )
+    }
+
+    @ExceptionHandler(ExpiredJwtException::class)
+    fun handleExpiredJwtException(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        ex: ExpiredJwtException
+    ): ResponseEntity<String> {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Expired JWT token")
+    }
+
+    @ExceptionHandler(MalformedJwtException::class)
+    fun handleMalformedJwtException(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        ex: MalformedJwtException
+    ): ResponseEntity<String> {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Malformed JWT token")
+    }
+
+    @ExceptionHandler(SignatureException::class)
+    fun handleSignatureException(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        ex: SignatureException
+    ): ResponseEntity<String> {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid signature in JWT token")
+    }
+
+    @ExceptionHandler(JwtException::class)
+    fun handleJwtException(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        ex: JwtException
+    ): ResponseEntity<String> {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Unknown JWT token error")
     }
 }
